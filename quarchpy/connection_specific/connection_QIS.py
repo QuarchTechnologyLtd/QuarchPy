809c2516ef3cf3eacb55f2db0513f833838b970d
mdearman <mdearman@265bcc82-57df-11dd-8522-2d0c19f12d9e>
2023-07-28 10:56:05 +0000

    minor fix
    git-svn-id: https://quarchserver4.ds.quarch.com/svn/Software/Python/QuarchPy/Trunk@7838 265bcc82-57df-11dd-8522-2d0c19f12d9e

--- quarchpy/connection_specific/connection_QIS.py
+++ quarchpy/connection_specific/connection_QIS.py
@@ -351,7 +351,7 @@ class QisInterface:
                     self.sendAndReceiveCmd(self.streamSock, 'rec stop', device=module, betweenCommandDelay = 0)
                     streamState = self.sendAndReceiveCmd(self.streamSock, 'stream?', device=module, betweenCommandDelay=0)  # use "stream?" rather than "rec stream?" as it checks both QIS AND the device.
                     while "stopped" not in streamState.lower():
-                        logging.debug("waiting for stream? to contained stopped")
+                        logging.debug("waiting for stream? to return stopped")
                         time.sleep(0.1)
                         streamState = self.sendAndReceiveCmd(self.streamSock, 'stream?', device=module, betweenCommandDelay=0)  # use "stream?" rather than "rec stream?" as it checks both QIS AND the device.
 

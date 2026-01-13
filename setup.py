14624934e8c214c31d8da7f4b5b5cd09c8e98fed
Nabil <nabil.ghayyda@quarch.com>
2026-01-13 11:18:29 +0000

    fix: add quarchpy-binaries in setup.py requirements

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(name='quarchpy',
             'numpy',
             'pandas',
             'requests',
-            'packaging'],
+            'packaging',
+            'quarchpy-binaries'],
       include_package_data=True,
       zip_safe=False)
